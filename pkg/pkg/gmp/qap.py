f6922ed2b95e7fb4177d59cde4a18e7349d978b1
bdpedigo <benjamindpedigo@gmail.com>
2021-03-02 11:25:39 -0500

    small qap modifications

--- pkg/pkg/gmp/qap.py
+++ pkg/pkg/gmp/qap.py
@@ -1,9 +1,10 @@
-import numpy as np
 import operator
-from scipy.optimize import linear_sum_assignment, OptimizeResult
 
+import numpy as np
 from scipy._lib._util import check_random_state
-import itertools
+from scipy.optimize import OptimizeResult, linear_sum_assignment
+
+from ..utils import _check_init_input, _doubly_stochastic, _split_matrix
 
 
 def quadratic_assignment(A, B, method="faq", options=None):
@@ -476,51 +477,3 @@ def _quadratic_assignment_faq(
     res = {"col_ind": unshuffled_perm, "fun": score, "nit": n_iter}
 
     return OptimizeResult(res)
-
-
-def _check_init_input(P0, n):
-    row_sum = np.sum(P0, axis=0)
-    col_sum = np.sum(P0, axis=1)
-    tol = 1e-3
-    msg = None
-    if P0.shape != (n, n):
-        msg = "`P0` matrix must have shape m' x m', where m'=n-m"
-    elif (
-        (~np.isclose(row_sum, 1, atol=tol)).any()
-        or (~np.isclose(col_sum, 1, atol=tol)).any()
-        or (P0 < 0).any()
-    ):
-        msg = "`P0` matrix must be doubly stochastic"
-    if msg is not None:
-        raise ValueError(msg)
-
-
-def _split_matrix(X, n):
-    # definitions according to Seeded Graph Matching [2].
-    upper, lower = X[:n], X[n:]
-    return upper[:, :n], upper[:, n:], lower[:, :n], lower[:, n:]
-
-
-def _doubly_stochastic(P, tol=1e-3):
-    # Adapted from @btaba implementation
-    # https://github.com/btaba/sinkhorn_knopp
-    # of Sinkhorn-Knopp algorithm
-    # https://projecteuclid.org/euclid.pjm/1102992505
-
-    max_iter = 1000
-    c = 1 / P.sum(axis=0)
-    r = 1 / (P @ c)
-    P_eps = P
-
-    for it in range(max_iter):
-        if (np.abs(P_eps.sum(axis=1) - 1) < tol).all() and (
-            np.abs(P_eps.sum(axis=0) - 1) < tol
-        ).all():
-            # All column/row sums ~= 1 within threshold
-            break
-
-        c = 1 / (r @ P)
-        r = 1 / (P @ c)
-        P_eps = r[:, None] * P * c
-
-    return P_eps
---
+++ pkg/pkg/gmp/utils.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+def _check_init_input(P0, n):
+    row_sum = np.sum(P0, axis=0)
+    col_sum = np.sum(P0, axis=1)
+    tol = 1e-3
+    msg = None
+    if P0.shape != (n, n):
+        msg = "`P0` matrix must have shape m' x m', where m'=n-m"
+    elif (
+        (~np.isclose(row_sum, 1, atol=tol)).any()
+        or (~np.isclose(col_sum, 1, atol=tol)).any()
+        or (P0 < 0).any()
+    ):
+        msg = "`P0` matrix must be doubly stochastic"
+    if msg is not None:
+        raise ValueError(msg)
+
+
+def _split_matrix(X, n):
+    # definitions according to Seeded Graph Matching [2].
+    upper, lower = X[:n], X[n:]
+    return upper[:, :n], upper[:, n:], lower[:, :n], lower[:, n:]
+
+
+def _doubly_stochastic(P, tol=1e-3, max_iter=1000):
+    # Adapted from @btaba implementation
+    # https://github.com/btaba/sinkhorn_knopp
+    # of Sinkhorn-Knopp algorithm
+    # https://projecteuclid.org/euclid.pjm/1102992505
+
+    c = 1 / P.sum(axis=0)
+    r = 1 / (P @ c)
+    P_eps = P
+
+    for it in range(max_iter):
+        if (np.abs(P_eps.sum(axis=1) - 1) < tol).all() and (
+            np.abs(P_eps.sum(axis=0) - 1) < tol
+        ).all():
+            # All column/row sums ~= 1 within threshold
+            break
+
+        c = 1 / (r @ P)
+        r = 1 / (P @ c)
+        P_eps = r[:, None] * P * c
+
+    return P_eps
